a6b779ff0c29196e8330a8ede9195f364c9968aa
KobiDouglasCook <kobidouglascook@gmail.com>
2026-07-01 23:06:13 -0400

    added IAM permissions

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,1 +1,80 @@
+// Define an execution role for Lambda function
+resource "aws_iam_role" "lambda_role" {
+  name = "${local.prefix}-lambda-role"
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+// Attach an AWS-managed policy to the Lambda role
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+// Create inline policy for Lambda role and attaches directly to the Lambda role
+resource "aws_iam_role_policy" "lambda_s3_glue" {
+  name = "${local.prefix}-lambda-policy"
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["s3:*"]
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["glue:StartJobRun"]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+// Create execution role for Glue Jobs
+resource "aws_iam_role" "glue_role" {
+  name = "${local.prefix}-glue-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "glue.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+// Attach AWS Managed Glue Role to the previously made Glue Role
+resource "aws_iam_role_policy_attachment" "glue_service" {
+  role       = aws_iam_role.glue_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
+
+// Add inline policy to the Glue Role to allow S3 actions
+resource "aws_iam_role_policy" "glue_s3" {
+  name = "${local.prefix}-glue-s3"
+  role = aws_iam_role.glue_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["s3:*"]
+      Resource = "*"
+    }]
+  })
+}
